782c4bd3418ed677544e651861539d4db3236f73
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-28 13:20:12 +0100

    change instruction if

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let fontSize = 10
 btn.addEventListener("click", () => {
     fontSize++
     for (let i = 0; i < li.length; i++) {
-        if (li[i].style.display === "") {
+        if (!li[i].style.display) {
             li[i].style.display = "block"
         }
         li[i].style.fontSize = `${fontSize}px`
