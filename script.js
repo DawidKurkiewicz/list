1e8bd0ceae0b0971712779887b602b51ca69ed6c
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-28 13:17:57 +0100

    li grows

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const btn = document.querySelector("button")
-
 const li = document.querySelectorAll("li")
+let fontSize = 10
 
 btn.addEventListener("click", () => {
+    fontSize++
     for (let i = 0; i < li.length; i++) {
         li[i].style.display = "block"
-        li[i].style.fontSize = "10px"
+        li[i].style.fontSize = `${fontSize}px`
     }
 })
