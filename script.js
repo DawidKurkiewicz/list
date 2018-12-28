cb0a55b82f09d37c79d062e1994ffdccfaad0c24
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-28 13:15:21 +0100

    display list

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// tutaj rozwiązanie
+const btn = document.querySelector("button")
+
+const li = document.querySelectorAll("li")
+
+btn.addEventListener("click", () => {
+    for (let i = 0; i < li.length; i++) {
+        li[i].style.display = "block"
+        li[i].style.fontSize = "10px"
+    }
+})
