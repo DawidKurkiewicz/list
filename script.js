444cf3f5167b4690414fa9b90d9f1ab99fa239a6
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-28 13:24:16 +0100

    same task with forEach

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const btn = document.querySelector("button")
 const li = document.querySelectorAll("li")
 let fontSize = 10
+// FOR
+// btn.addEventListener("click", () => {
+//     for (let i = 0; i < li.length; i++) {
+//         if (!li[i].style.display) {
+//             li[i].style.display = "block"
+//         }
+//         li[i].style.fontSize = `${fontSize}px`
+//     }
+//     fontSize++
 
+// })
+
+//FOREACH
 btn.addEventListener("click", () => {
+    li.forEach((liitem) => {
+        liitem.style.display = "block"
+        liitem.style.fontSize = `${fontSize}px`
+    })
     fontSize++
-    for (let i = 0; i < li.length; i++) {
-        if (!li[i].style.display) {
-            li[i].style.display = "block"
-        }
-        li[i].style.fontSize = `${fontSize}px`
-    }
+
 })
+
